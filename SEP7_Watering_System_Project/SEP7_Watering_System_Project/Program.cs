adfd834e1de09adecc051f8834c7bfb33f98dc77
Hawo <tomasdurnek98@gmail.com>
2020-11-24 11:09:39 +0100

    Creation of the project

---
+++ SEP7_Watering_System_Project/SEP7_Watering_System_Project/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SEP7_Watering_System_Project
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
