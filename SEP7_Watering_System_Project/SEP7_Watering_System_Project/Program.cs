a40f2aa1601ed21c72d92b3fb3121851c0a86431
Hawo <tomasdurnek98@gmail.com>
2020-11-24 11:19:50 +0100

    Test1

--- SEP7_Watering_System_Project/SEP7_Watering_System_Project/Program.cs
+++ SEP7_Watering_System_Project/SEP7_Watering_System_Project/Program.cs
@@ -1,4 +1,8 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace SEP7_Watering_System_Project
 {
@@ -6,7 +10,6 @@ namespace SEP7_Watering_System_Project
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
         }
     }
 }
